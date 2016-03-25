4ed5c522bae41df6a5e25fb4dc7e5cc33a261573
Dustin Bensing <dustin.bensing@googlemail.com>
2016-03-25 21:50:36 +0100

    fixed vector insert bug

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,17 @@ use orbclient::window::EventIter;
 use std::time::Instant;
 
 struct Vertex {
-    x: f32,
-    y: f32
+    x: i32,
+    y: i32
 }
 
 impl Vertex {
-    pub fn calc_area(&self, v1: &Vertex, v2: &Vertex) -> f32 {
+    pub fn calc_double_area(&self, v1: &Vertex, v2: &Vertex) -> i32 {
 
-        let x1 = v1.x - self.x;
-        let y1 = v1.y - self.y;
-        let x2 = v2.x - self.x;
-        let y2 = v2.y - self.y;
+        let x1 = (v1.x - self.x) as i32;
+        let y1 = (v1.y - self.y) as i32;
+        let x2 = (v2.x - self.x) as i32;
+        let y2 = (v2.y - self.y) as i32;
 
         (x1 * y2 - x2 * y1)
     }
@@ -23,7 +23,7 @@ impl Vertex {
 
 struct RenderContext {
     window: Box<orbclient::Window>,
-    scan_buffer: Vec<u32> //TODO(dustin): do i need Vec<32> here? [i32]
+    scan_buffer: Vec<i32> //TODO(dustin): do i need Vec<32> here? [i32]
 }
 
 impl RenderContext {
@@ -68,31 +68,31 @@ impl RenderContext {
             mid_vert = tmp;
         }
 
-        let area = min_vert.calc_area(&max_vert, &mid_vert);
-        let side = if area >= 0f32 { 1 } else { 0 };
+        let area = min_vert.calc_double_area(&max_vert, &mid_vert);
+        let side = if area >= 0 { 1 } else { 0 };
         self.convert_triangle(&min_vert, &mid_vert, &max_vert, side);
-        self.fill_convex_shape(min_vert.y as u32, max_vert.y as u32);
+        self.fill_convex_shape(min_vert.y, max_vert.y);
     }
 
-    fn fill_convex_shape(&mut self, y_min: u32, y_max: u32) {
+    fn fill_convex_shape(&mut self, y_min: i32, y_max: i32) {
 
         for y_idx in y_min..y_max {
             let x_min = self.scan_buffer.get((y_idx * 2) as usize).unwrap().clone();
             let x_max = self.scan_buffer.get((y_idx * 2 + 1) as usize).unwrap().clone();
 
             for x_idx in x_min..x_max {
-                self.window.pixel(x_idx as i32, y_idx as i32, orbclient::Color { data: 0xFFFFFFFF });
+                self.window.pixel(x_idx, y_idx, orbclient::Color { data: 0xFFFFFFFF });
             }
         }
     }
 
     fn convert_triangle(&mut self, min_vert: &Vertex, mid_vert: &Vertex, max_vert: &Vertex, side: i32) {
-		self.convert_line(min_vert, max_vert, 0 + side);
-		self.convert_line(min_vert, mid_vert, 1 - side);
-		self.convert_line(mid_vert, max_vert, 1 - side);
-	}
+        self.convert_line(min_vert, max_vert, 0 + side);
+        self.convert_line(min_vert, mid_vert, 1 - side);
+        self.convert_line(mid_vert, max_vert, 1 - side);
+    }
 
-	fn convert_line(&mut self, min_vert: &Vertex, max_vert: &Vertex, side: i32) {
+    fn convert_line(&mut self, min_vert: &Vertex, max_vert: &Vertex, side: i32) {
         let start_y = min_vert.y;
         let start_x = min_vert.x;
         let end_y = max_vert.y;
@@ -101,18 +101,18 @@ impl RenderContext {
         let dist_y = end_y - start_y;
         let dist_x = end_x - start_x;
 
-		if dist_y <= 0f32 {
-			return;
-		}
-
-		let setp_x = dist_x as f32 / dist_y as f32;
-		let mut current_x = start_x;
-
-        for y_coord in start_y as i32..end_y as i32 {
-            self.scan_buffer.insert((y_coord * 2 + side) as usize, current_x as u32);
-            current_x += setp_x;
+        if dist_y <= 0 {
+            return;
         }
-	}
+
+        let step_x = dist_x as f32 / dist_y as f32;
+        let mut current_x = start_x as f32;
+
+        for y_coord in start_y..end_y {
+            self.scan_buffer[((y_coord * 2 + side) as usize)] = current_x as i32;
+            current_x += step_x;
+        }
+    }
 
 }
 
@@ -121,9 +121,9 @@ fn main() {
     let mut render_context = RenderContext::new(500, 400, "pixelcannon");
     let mut start = Instant::now();
 
-    let min_vert = Vertex{x: 100f32, y: 50f32};
-    let mid_vert = Vertex{x: 200f32, y: 200f32};
-    let max_vert = Vertex{x: 70f32, y: 300f32};
+    let min_vert = Vertex{x: 250, y: 0};
+    let mid_vert = Vertex{x: 0, y: 200};
+    let max_vert = Vertex{x: 500, y: 300};
 
     'event: loop {
 
@@ -135,7 +135,7 @@ fn main() {
             // println!("{} ms", delta_ms);
 
             render_context.clear();
-            render_context.draw_triangle(&max_vert, &mid_vert, &min_vert);
+            render_context.draw_triangle(&min_vert, &mid_vert, &max_vert);
             render_context.sync();
         }
 
